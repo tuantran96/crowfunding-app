312eb8f5ef5923c23fa2483a5407d1525b83888a
tuantran <tuannnnnn1996@gmail.com>
2023-07-24 21:10:13 +0700

    bai 373

---
+++ src/pages/SignInPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutAuthentication from "../layout/LayoutAuthentication";
+
+const SignInPage = () => {
+  return (
+    <div>
+      <LayoutAuthentication heading="Sign In"></LayoutAuthentication>
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutAuthentication from "../layout/LayoutAuthentication";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <LayoutAuthentication heading="Sign Up"></LayoutAuthentication>
+    </div>
+  );
+};
+
+export default SignUpPage;
