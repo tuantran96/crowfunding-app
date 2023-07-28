43e13bfbf151fa8b8bfd1aa7b3d838d92b7dc71a
tuantran <tuannnnnn1996@gmail.com>
2023-07-28 21:56:48 +0700

    bai 381

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 
 const SignUpPage = lazy(() => import("./pages/SignUpPage"));
 const SignInPage = lazy(() => import("./pages/SignInPage"));
+const DashboardPage = lazy(() => import("./pages/DashboardPage"));
 
 function App() {
   return (
     <Suspense>
       <Routes>
+        <Route path="/" element={<DashboardPage></DashboardPage>}></Route>
         <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
         <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
       </Routes>
---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,15 @@
+import LayoutDashboard from "layout/LayoutDashboard";
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <LayoutDashboard>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque quisquam
+      saepe corporis dicta. Sunt dignissimos autem expedita praesentium,
+      corrupti labore quia voluptatem eaque voluptates, consequuntur repellat a
+      necessitatibus odio accusantium?
+    </LayoutDashboard>
+  );
+};
+
+export default DashboardPage;
