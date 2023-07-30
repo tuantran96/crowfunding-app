c3027186bb4c08e531363f70937e6ee02bcaf415
tuantran <tuannnnnn1996@gmail.com>
2023-07-30 14:57:41 +0700

    bai 385

---
+++ src/components/common/Overlay.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Overlay = () => {
+  return (
+    <div className="overlay fixed inset-0 z-40 bg-black bg-opacity-10 opacity-0 invisible"></div>
+  );
+};
+
+export default Overlay;
--- src/layout/LayoutDashboard.js
+++ src/layout/LayoutDashboard.js
@@ -1,3 +1,4 @@
+import Overlay from "components/common/Overlay";
 import DashboardSidebar from "modules/dashboard/DashboardSidebar";
 import DashboardTopbar from "modules/dashboard/DashboardTopbar";
 import React from "react";
@@ -5,6 +6,7 @@ import React from "react";
 const LayoutDashboard = ({ children }) => {
   return (
     <div className="p-10 bg-lite min-h-screen">
+      <Overlay></Overlay>
       <DashboardTopbar></DashboardTopbar>
       <div className="flex items-start gap-x-10">
         <DashboardSidebar></DashboardSidebar>
